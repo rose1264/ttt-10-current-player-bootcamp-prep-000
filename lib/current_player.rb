ae894fd6eea8d867be638d953a4bc421dec1f02f
Shumei Huang <rose1264@gmail.com>
2017-10-24 14:10:20 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,7 +1,13 @@
-def turn_count
-
+def turn_count(board)
+  turns = 0
+  board.each do |token|
+    if token == X || token == O
+      turn += 1
+    end
+  end
+  return turns
 end
 
-def current_player
-
+def current_player(board)
+  turn_count(board) % 2 == 0 ? "X" : "O"
 end
